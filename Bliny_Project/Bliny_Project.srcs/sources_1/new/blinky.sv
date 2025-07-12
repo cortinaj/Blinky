073ba4dcf274a7607d222a7694de9080a840f310
cortinaj <johnpaul.cortina.716@my.csun.edu>
2025-07-11 18:25:18 -0700

    John Paul Cortina - 7/11/2025 - Imported constraint file from digilent, created blinky source file using system verilog as target language

---
+++ Bliny_Project/Bliny_Project.srcs/sources_1/new/blinky.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/11/2025 06:21:30 PM
+// Design Name: 
+// Module Name: blinky
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module blinky(
+    input clk,
+    output led
+    );
+endmodule
